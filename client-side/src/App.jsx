6c642ec8fde88bfaa5614a37a6386e5f43bbbce7
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-23 01:49:18 +0500

    Pages and routes setting

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Routes, Route, BrowserRouter, Router } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Project'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+          <Route path="/signup" element={<Signup/>}></Route>
+          <Route path="/signin" element={<Signin/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
   )
 }
