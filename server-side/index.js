da82b565310d2e2684e7b9f6fa8c328a64ad1e38
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-23 17:11:51 +0500

    setting-up-test-route

--- server-side/index.js
+++ server-side/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import { connectDB } from './connection.js'
+import userRoutes from './routes/user.routes.js'
 
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
 connectDB()
+
+app.use('/api/user', userRoutes)
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000!!")
 })
+
