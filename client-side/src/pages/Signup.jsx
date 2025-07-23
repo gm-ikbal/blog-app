04ffc849b14fbda92563046fb3d6f7ff05e88318
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-23 22:59:55 +0500

    SignUp Form functionality

--- client-side/src/pages/Signup.jsx
+++ client-side/src/pages/Signup.jsx
@@ -1,48 +1,71 @@
 /* eslint-disable no-unused-vars */
-import { Alert, Button, Label, Spinner, TextInput} from 'flowbite-react';
-import { Form, Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { useState } from 'react';
 // import OAuth from '../components/OAuth';
 
 export default function Signup() {
   const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+
+  const navigate = useNavigate();
+  
   const handleChange = (e) => {
-  setFormData({...formData , [e.target.id]: e.target.value})
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
+  const handleSubmit = async (e) => {
 
-const handleSubmit = async(e)=> {
-  e.preventDefault();
-  const res = await fetch("/user/registration", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  });
-  const data = await res.json();
-}
+    e.preventDefault();
+    setErrorMessage(null);
+    setLoading(true);
+    try {
+      setLoading(true)
+      const res = await fetch("/user/registration", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setErrorMessage(data.message || "Signup failed");
+      } else {
+        setErrorMessage(null);
+        setFormData({});
+        navigate("/sign-in");
+      }
+
+      setLoading(false)
+    } catch (error) {
+      setErrorMessage("Something went wrong. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+   
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
-      <div className="flex-1">
+        <div className="flex-1">
           <Link
-                to="/"
-                className="font-bold dark:text-white text-4xl"
-            >
-                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Sign Up
-                </span>
-            </Link>
-            <p className="text-sm mt-5">
+            to="/"
+            className="font-bold dark:text-white text-4xl"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Sign Up
+            </span>
+          </Link>
+          <p className="text-sm mt-5">
             This is a demo project. You can sign up with your email and password
-            </p>
-          </div>
+          </p>
+        </div>
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value='Your username'/>
+              <Label value='Your username' />
               <TextInput
                 type='text'
                 placeholder='Username'
@@ -51,7 +74,7 @@ const handleSubmit = async(e)=> {
               />
             </div>
             <div>
-              <Label value='Your email'/>
+              <Label value='Your email' />
               <TextInput
                 type='email'
                 placeholder='name@company.com'
@@ -65,13 +88,23 @@ const handleSubmit = async(e)=> {
                 type='password'
                 placeholder='Password'
                 id='password'
-              onChange={handleChange}
+                onChange={handleChange}
               />
             </div>
-            <Button type="submit">
-Sign Up
+            <Button 
+            type="submit"
+             disabled={loading}
+              >
+                {
+                  loading ? (
+                    <>
+                    <Spinner size="sm" />
+                    <span className="pl-3">Loading...</span>
+                    </>
+
+                  ) : ("Sign Up")
+                  }
             </Button>
-      
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
@@ -79,7 +112,11 @@ Sign Up
               Sign In
             </Link>
           </div>
-
+          {errorMessage && (
+              <Alert className="mt-5" color="failure">
+                {errorMessage}
+              </Alert>
+            )}
         </div>
       </div>
     </div>
