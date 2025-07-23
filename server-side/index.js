2fffe8758c5ea96becd0213aa909ca53ef5d0fb2
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-23 16:28:24 +0500

    db-connection

--- server-side/index.js
+++ server-side/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
-const app = express()
+import { connectDB } from './connection.js'
 
-app.listen(3000 , ()=>{
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+connectDB()
+app.listen(3000, () => {
     console.log("Server is running on port 3000!!")
 })
