e6ebca9161d0e8b94a5324981957902e750a9a83
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-24 20:38:15 +0500

    google Auth functionality

--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { store, persistor } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from '../src/redux/store'
-import { Provider } from 'react-redux'
-
-createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-  <App/>
- </Provider>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      
+        <App />
+    
+    </Provider>
+  </PersistGate>
+);
--- client-side/src/pages/Signin.jsx
+++ client-side/src/pages/Signin.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { signInStart, signInSuccess,signInFailure } from '../redux/user/userSlice';
 import { useDispatch ,useSelector} from 'react-redux';
 import { useState } from 'react';
+import OAuth from '../component/OAuth';
 // import OAuth from '../components/OAuth';
 
 export default function Signin() {
@@ -103,6 +104,7 @@ export default function Signin() {
                 ) : ("Sign In")
               }
             </Button>
+            <OAuth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an Account?</span>
