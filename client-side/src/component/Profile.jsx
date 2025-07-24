ee03486532789ffa77d1bce3c0fa97aa47b30380
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-25 03:24:07 +0500

    profile upload client side

--- client-side/src/component/Profile.jsx
+++ client-side/src/component/Profile.jsx
@@ -1,25 +1,20 @@
+/* eslint-disable no-unused-vars */
 import { TextInput, Button } from 'flowbite-react';
-import React from 'react'
+import React, { useRef } from 'react'
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
 
 export default function Profile() {
     const { currentUser } = useSelector((state) => state.user);
-     const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
-  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
-  const [imageFileUploadError, setImageFileUploadError] = useState(null);
-  const [imageFileUploading, setImageFileUploading] = useState(false);
-  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
-  const [updateUserError, setUpdateUserError] = useState(null);
-  const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState({});
+  const [imageFile, setImageFile] = useState(null);
 
+const filePickerRef = useRef(null);
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      setImageFile(file);
-      setImageFileUrl(URL.createObjectURL(file));
+     setImageFile(file);
+     setImageFileUrl(URL.createObjectURL(file));
     }
   };
 
@@ -27,16 +22,21 @@ export default function Profile() {
         <div className='max-w-lg mx-auto p-3 w-full'>
             <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
             <form className='flex flex-col gap-4'>
-        <input
-          type='file'
-          accept='image/*'
-          onChange={handleImageChange}
-          //ref={filePickerRef}
-          hidden
-        />               
-                <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+            <div className="flex items-center gap-4">
+
+            <input
+    id="imageUpload"
+    type="file"
+    accept="image/*"
+    onChange={handleImageChange}
+    className="hidden"
+    ref={filePickerRef}
+  />
+</div>
+             
+                <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
                     <img
-                        src={currentUser["profile Picture"]}
+                        src={ imageFileUrl || currentUser["profile Picture"]}
                         alt="user"
                         className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
                     />
