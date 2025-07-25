616e18a85287e7d9e8eb8440e8880d02dbf81e81
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-25 16:12:07 +0500

    user profile

--- client-side/src/component/Profile.jsx
+++ client-side/src/component/Profile.jsx
@@ -5,47 +5,47 @@ import { useState } from 'react';
 
 export default function Profile() {
     const { currentUser } = useSelector((state) => state.user);
-  const [imageFileUrl, setImageFileUrl] = useState(null);
-  const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFile, setImageFile] = useState(null);
 
-const filePickerRef = useRef(null);
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-     setImageFile(file);
-     setImageFileUrl(URL.createObjectURL(file));
-    }
-  };
-  
-useEffect(()=>{
-    if(imageFile){
-        uploadImage();
-    }
-},[imageFile])
+    const filePickerRef = useRef(null);
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+    };
 
-const uploadImage = async () => {
-   console.log("uploadin..")
-}
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile])
+
+    const uploadImage = async () => {
+        console.log("uploadin..")
+    }
 
     return (
         <div className='max-w-lg mx-auto p-3 w-full'>
             <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
             <form className='flex flex-col gap-4'>
-            <div className="flex items-center gap-4">
+                <div className="flex items-center gap-4">
 
-            <input
-    id="imageUpload"
-    type="file"
-    accept="image/*"
-    onChange={handleImageChange}
-    className="hidden"
-    ref={filePickerRef}
-  />
-</div>
-             
+                    <input
+                        id="imageUpload"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        className="hidden"
+                        ref={filePickerRef}
+                    />
+                </div>
+
                 <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
                     <img
-                        src={ imageFileUrl || currentUser["profile Picture"]}
+                        src={imageFileUrl || currentUser["profile Picture"]}
                         alt="user"
                         className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
                     />
