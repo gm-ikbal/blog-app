6d5a21b8d69f890805de640e9fed5a22935c2d8f
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-26 04:10:02 +0500

    updated get posts

--- server-side/routes/post.routes.js
+++ server-side/routes/post.routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { createPost, getPosts } from '../controllers/post.controller.js'
+import { createPost, getposts } from '../controllers/post.controller.js'
 import verifyUser from '../Utils/verify.js'
 const router = express.Router()
 
 router.post('/createpost', verifyUser, createPost)
-router.get('/getposts', verifyUser, getPosts)
+router.get('/getposts', verifyUser, getposts)
 
 export default router
