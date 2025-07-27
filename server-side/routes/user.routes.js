81e81ab9a2c650733d1be9677791ed8df5a40e0d
gm-ikbal <xsmohiuddin@gmail.com>
2025-07-27 18:11:48 +0500

    get userApi

--- server-side/routes/user.routes.js
+++ server-side/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import verifyUser from '../Utils/verify.js'
-import { userUpdate, userDelete, userSignOut } from '../controllers/user.controller.js'
+import { userUpdate, userDelete, userSignOut, getUser } from '../controllers/user.controller.js'
 const router = express.Router()
 
 
 router.put('/update/:userId', verifyUser, userUpdate)
 router.delete('/delete/:userId', verifyUser, userDelete)
 router.post('/signout', userSignOut)
+router.get('/getuser', verifyUser, getUser)
 
 export default router
